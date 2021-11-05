3297c837f48a2a3c2a9e401034754618f5ed1fb7
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2021-11-05 13:28:42 -0500

    changes sample_profiles.yml to profile_template.yml to match v1 changes

--- {{cookiecutter.project_name}}/dbt/adapters/{{cookiecutter.directory_name}}/impl.py
+++ {{cookiecutter.project_name}}/dbt/adapters/{{cookiecutter.directory_name}}/impl.py
@@ -5,10 +5,17 @@ from dbt.adapters.{{cookiecutter.adapter}} import {{cookiecutter.adapter_title}}
 
 
 class {{cookiecutter.adapter_title}}Adapter({{cookiecutter.adapter_cls}}):
+    '''
+    Controls actual implmentation of adapter, and ability to override certain methods.
+    '''
+    
     ConnectionManager = {{cookiecutter.adapter_title}}ConnectionManager
 
     @classmethod
     def date_function(cls):
+        '''
+        Returns canonical date func
+        '''
         return 'datenow()'
 
  # may require more build out to make more user friendly to confer with team and community.

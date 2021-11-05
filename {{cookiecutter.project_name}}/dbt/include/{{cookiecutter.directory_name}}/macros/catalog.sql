04250f560a6bd8cd99170d1c9be933bab5b0f1b8
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2021-11-05 14:18:32 -0500

    added cookiecutter to requirements

--- {{cookiecutter.project_name}}/dbt/include/{{cookiecutter.directory_name}}/macros/catalog.sql
+++ {{cookiecutter.project_name}}/dbt/include/{{cookiecutter.directory_name}}/macros/catalog.sql
@@ -3,6 +3,14 @@
    {{'{{%'}}set msg {{ '-%}}' }}
     get_catalog not implemented for {{cookiecutter.adapter}}
    {{ '-%}}' }} endset {{'{{%'}}
+    /*
+      Your database likely has a way of accessing metadata about its objects,
+      whether by querying an information schema or by running `show` and `describe` commands. 
+      dbt will use this macro to generate its catalog of objects it knows about. The catalog is one of 
+      the artifacts powering the documentation site.
+      As an example, below is a simplified version of postgres__get_catalog
+    */
+
 
   {{ '{{{{ exceptions.raise_compiler_error(msg) }}}}' }}
  {{'{{%'}} endmacro {{'{{%'}}

d9a059807a9723722910d7640aa27b6f97421e90
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2021-11-08 11:50:42 -0600

    changed format to comments in adapter.sql and added psuedocode of how to build out methods based on default and postgres

--- {{cookiecutter.project_name}}/dbt/include/{{cookiecutter.directory_name}}/macros/adapters.sql
+++ {{cookiecutter.project_name}}/dbt/include/{{cookiecutter.directory_name}}/macros/adapters.sql
@@ -4,9 +4,20 @@ dbt docs: https://docs.getdbt.com/docs/contributing/building-a-new-adapter
 */
 
 {{'{%'}} macro {{cookiecutter.adapter}}__alter_column_type(relation,column_name,new_column_type) {{ '-%}' }}
+/*
+    1. Create a new column (w/ temp name and correct type)
+    2. Copy data over to it
+    3. Drop the existing column (cascade!)
+    4. Rename the new column to existing column
+*/
 {{'{%'}} endmacro {{ '%}' }}
 
 {{'{%'}} macro {{cookiecutter.adapter}}__check_schema_exists(information_schema,schema) {{ '-%}' }}
+/*
+    1. Check if schemas exist
+    2. verify database connection
+    3. return number of rows or columns that match searched parameter
+*/
 {{'{%'}} endmacro {{ '%}' }}
 
 --  Example from postgres adapter in dbt-core 
@@ -28,94 +39,134 @@ dbt docs: https://docs.getdbt.com/docs/contributing/building-a-new-adapter
 {{'{%'}} macro {{cookiecutter.adapter}}__create_schema(relation) {{ '-%}' }}
 {{'{%'}} endmacro {{ '%}' }}
 
--- {% raw %}
--- {% macro postgres__drop_schema(relation) -%}
---   {% if relation.database -%}
---     {{ adapter.verify_database(relation.database) }}
---   {%- endif -%}
---   {%- call statement('drop_schema') -%}
---     drop schema if exists {{ relation.without_identifier().include(database=False) }} cascade
---   {%- endcall -%}
--- {% endmacro %}
--- {% endraw %}
+/*
+{% raw %}
+{% macro postgres__drop_schema(relation) -%}
+  {% if relation.database -%}
+    {{ adapter.verify_database(relation.database) }}
+  {%- endif -%}
+  {%- call statement('drop_schema') -%}
+    drop schema if exists {{ relation.without_identifier().include(database=False) }} cascade
+  {%- endcall -%}
+{% endmacro %}
+{% endraw %}
+*/
 
 {{'{%'}} macro {{cookiecutter.adapter}}__drop_relation(relation) {{ '-%}' }}
+/*
+  1. If database exists
+  2. Verify connection
+  3. Create a new schema if passed schema does not exist already
+*/
 {{'{%'}} endmacro {{ '%}' }}
 
 {{'{%'}} macro {{cookiecutter.adapter}}__drop_schema(relation) {{ '-%}' }}
+/*
+  1. If database exists
+  2. werify database
+  3. search all calls of schema, and change include value to False, cascade it to backtrack
+*/
 {{'{%'}} endmacro {{ '%}' }}
 
--- {% raw %}
---  Example of 1 of 3 required macros that does not have a default implementation
--- {% macro postgres__get_columns_in_relation(relation) -%}
---   {% call statement('get_columns_in_relation', fetch_result=True) %}
---       select
---           column_name,
---           data_type,
---           character_maximum_length,
---           numeric_precision,
---           numeric_scale
+/*
+{% raw %}
+ Example of 1 of 3 required macros that does not have a default implementation
+{% macro postgres__get_columns_in_relation(relation) -%}
+  {% call statement('get_columns_in_relation', fetch_result=True) %}
+      select
+          column_name,
+          data_type,
+          character_maximum_length,
+          numeric_precision,
+          numeric_scale
 
---       from {{ relation.information_schema('columns') }}
---       where table_name = '{{ relation.identifier }}'
---         {% if relation.schema %}
---         and table_schema = '{{ relation.schema }}'
---         {% endif %}
---       order by ordinal_position
+      from {{ relation.information_schema('columns') }}
+      where table_name = '{{ relation.identifier }}'
+        {% if relation.schema %}
+        and table_schema = '{{ relation.schema }}'
+        {% endif %}
+      order by ordinal_position
 
---   {% endcall %}
---   {% set table = load_result('get_columns_in_relation').table %}
---   {{ return(sql_convert_columns_in_relation(table)) }}
--- {% endmacro %}
--- {% endraw %}
+  {% endcall %}
+  {% set table = load_result('get_columns_in_relation').table %}
+  {{ return(sql_convert_columns_in_relation(table)) }}
+{% endmacro %}
+{% endraw %}*/
+
 
 {{'{%'}} macro {{cookiecutter.adapter}}__get_columns_in_relation(relation) {{ '-%}' }}
+/*
+  1. select as many values from column as needed
+  2. search relations to columns
+  3. where table name is equal to the relation identifier
+  4. if a relation schema exists and table schema is equal to the relation schema
+  5. order in whatever way you want to call.
+  6. create a table by loading result from call
+  7. return new table
+*/
 {{'{%'}} endmacro {{ '%}' }}
 
 --  Example of 2 of 3 required macros that do not come with a default implementation
 
--- {% raw %}
--- {% macro postgres__list_relations_without_caching(schema_relation) %}
---   {% call statement('list_relations_without_caching', fetch_result=True) -%}
---     select
---       '{{ schema_relation.database }}' as database,
---       tablename as name,
---       schemaname as schema,
---       'table' as type
---     from pg_tables
---     where schemaname ilike '{{ schema_relation.schema }}'
---     union all
---     select
---       '{{ schema_relation.database }}' as database,
---       viewname as name,
---       schemaname as schema,
---       'view' as type
---     from pg_views
---     where schemaname ilike '{{ schema_relation.schema }}'
---   {% endcall %}
---   {{ return(load_result('list_relations_without_caching').table) }}
--- {% endmacro %}
--- {% endraw %}
+/*
+{% raw %}
+{% macro postgres__list_relations_without_caching(schema_relation) %}
+  {% call statement('list_relations_without_caching', fetch_result=True) -%}
+    select
+      '{{ schema_relation.database }}' as database,
+      tablename as name,
+      schemaname as schema,
+      'table' as type
+    from pg_tables
+    where schemaname ilike '{{ schema_relation.schema }}'
+    union all
+    select
+      '{{ schema_relation.database }}' as database,
+      viewname as name,
+      schemaname as schema,
+      'view' as type
+    from pg_views
+    where schemaname ilike '{{ schema_relation.schema }}'
+  {% endcall %}
+  {{ return(load_result('list_relations_without_caching').table) }}
+{% endmacro %}
+{% endraw %}
+*/
 
 {{'{%'}} macro {{cookiecutter.adapter}}__list_relations_without_caching(schema_relation) {{ '-%}' }}
 {{'{%'}} endmacro {{ '%}' }}
 
 {{'{%'}} macro {{cookiecutter.adapter}}__list_schemas(database) {{ '-%}' }}
+/*
+  1. Verify database
+  2. search schemea by specific name
+  3. create a table with names
+*/
 {{'{%'}} endmacro {{ '%}' }}
 
 {{'{%'}} macro {{cookiecutter.adapter}}__rename_relation(from_relation, to_relation) {{ '-%}' }}
+/*
+  1. Search for a specific relation name
+  2. alter table by targeting specific name and passing in new name
+*/
 {{'{%'}} endmacro {{ '%}' }}
 
 {{'{%'}} macro {{cookiecutter.adapter}}__truncate_relation(relation) {{ '-%}' }}
+/*
+  1. grab all tables tied to the relation
+  2. remove rows from relations
+*/
 {{'{%'}} endmacro {{ '%}' }}
 
--- {% raw %}
--- Example 3 of 3 of required macros that does not have a default implementation.
---  ** Good example of building out small methods ** please refer to impl.py for implementation of now() in postgres plugin
--- {% macro postgres__current_timestamp() -%}
---   now()
--- {%- endmacro %}
--- {% endraw %}
+/*
+{% raw %}
+Example 3 of 3 of required macros that does not have a default implementation.
+ ** Good example of building out small methods ** please refer to impl.py for implementation of now() in postgres plugin
+{% macro postgres__current_timestamp() -%}
+  now()
+{%- endmacro %}
+{% endraw %}
+*/
 
 {{'{%'}} macro {{cookiecutter.adapter}}__current_timestamp() {{ '-%}' }}
 {{'{#'}} docs show not to be implemented currently. {{'#}'}}

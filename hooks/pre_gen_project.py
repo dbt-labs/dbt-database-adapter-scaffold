0cef59d1124de780327bd688a6818071ebb28a0d
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2021-11-17 14:16:26 -0600

    built out 1st draft version of dev_requirements, tox.ini, and figured out how to update cookiecutter.json using other cookiecutter values (will be helpful in minimizing the interactive part for users)

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,5 +1,12 @@
+{{ cookiecutter.update({ "adapter_upper":cookiecutter.directory_name.upper() }) }}
+
+
 {% if cookiecutter.adapter_src == "sql" %}
 {{ cookiecutter.update({ "adapter_cls":"SQLAdapter","connection_cls": "SQLConnectionManager"}) }}
 {% else %}
 {{ cookiecutter.update({"adapter_cls":"BaseAdapter","connection_cls":"BaseConnectionManager"}) }}
 {% endif %}
+
+
+
+

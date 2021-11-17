8693b4b1315eceb32d5c7099408ab6fe900ba56e
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2021-11-17 15:49:48 -0600

    typo fix

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,6 +1,5 @@
 {{ cookiecutter.update({ "adapter_upper":cookiecutter.directory_name.upper() }) }}
 
-
 {% if cookiecutter.adapter_src == "sql" %}
 {{ cookiecutter.update({ "adapter_cls":"SQLAdapter","connection_cls": "SQLConnectionManager"}) }}
 {% else %}

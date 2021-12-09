1a15e90c192fc6a803847f9d1d91381ebedc2d1f
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2021-12-09 14:57:40 -0600

    created two basic tests one with same as default values the other without.

--- tests/template_test.py
+++ tests/template_test.py
@@ -2,23 +2,45 @@ from contextlib import contextmanager
 import pytest
 from cookiecutter.utils import rmtree
 
-@contextmanager
-def bake_in_temp_dir(cookies, *args, **kwargs):
-    extra_context = kwargs.setdefault('extra_context',{})
-    extra_context.setdefault('project_name','myproject')
-    result = cookies.bake(*args, **kwargs)
-    try:
-        yield result
-    finally: 
-        rmtree(str(result.project))
+# @contextmanager
+# def bake_in_temp_dir(cookies, *args, **kwargs):
+#     extra_context = kwargs.setdefault('extra_context',{})
+#     extra_context.setdefault('project_name','myproject')
+#     result = cookies.bake(*args, **kwargs)
+#     try:
+#         yield result
+#     finally: 
+#         rmtree(str(result.project))
 
-def test_bake_with_defaults(cookies):
-    with bake_in_temp_dir(cookies) as result:
-        assert result.exit_code == 0
-        assert result.exception is None
-        assert result.project.isdir()
-        found_toplevel_files = [f.basename for f in result.project.listdir()]
-        assert 'test' in found_toplevel_files
-        assert 'setup.py'in found_toplevel_files
+def test_bake_project(cookies):
+    result = cookies.bake()
+
+    assert result.exit_code == 0
+    assert result.exception is None
+
+    assert result.project_path.name == "dbt-myadapter"
+    assert result.project_path.is_dir()
+
+    # The `project` attribute is deprecated
+    assert result.project.basename == "dbt-myadapter"
+    assert result.project.isdir()
+
+
+def test_bake_project_fail(cookies):
+    result = cookies.bake()
+
+    assert result.exit_code == 0
+    assert result.exception is None
+
+    assert result.project_path.name != "hello_world"
+    assert result.project_path.is_dir()
+
+    # The `project` attribute is deprecated
+    assert result.project.basename != "hello_world"
+    assert result.project.isdir()
+
+
+
+    
 
 

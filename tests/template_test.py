fe434d7235f1b669969e6e1c9e9dcb2fb0d86625
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2021-12-14 15:49:22 -0600

    some base tests created for most of cookiecutter.json, remaining tests to determine are for packaging/versioning, and known issues around testing conditionally rendered variables to work on if possible though unlikely.

--- tests/template_test.py
+++ tests/template_test.py
@@ -1,53 +1,76 @@
-import json
-import pytest
+import re
 from contextlib import contextmanager
 from cookiecutter.utils import rmtree
+from cookiecutter.hooks import run_script_with_context
+
 
 @contextmanager
 def bake_in_temp_dir(cookies, *args, **kwargs):
-    extra_context = kwargs.setdefault('extra_context', {})
-    extra_context.setdefault('adapter_name', 'helloworld')
     result = cookies.bake(*args, **kwargs)
     try:
         yield result
     finally:
-        rmtree(str(result.project_path))
+        rmtree(str(result.project_path)) 
 
-def test_bake_project_default(cookies):
-    """bake and test against default values of cookiecutter.json"""
-    result = cookies.bake()
-    assert result.exit_code == 0
-    assert result.exception is None
-    assert result.project_path.name == "dbt-myadapter"
-    assert result.project_path.is_dir()
-    # looks for files in generated project
-    found_toplevel_files = [f.name for f in result.project_path.glob('*')]
-    assert  'dbt' in found_toplevel_files
-    assert 'setup.py' in found_toplevel_files
-    assert 'tox.ini' in found_toplevel_files
-    assert 'test' in found_toplevel_files
+def test_bake_project_default_adapter_name(cookies):
+    """bake and test against default values of cookiecutter.json, adapter_name, and top level files created"""
+    with bake_in_temp_dir(cookies) as result:
+        assert result.exit_code == 0
+        assert result.exception is None
+        assert result.context['adapter_name'] == "My Adapter"
+        assert result.context['adapter_name'] != '123444'
+        assert result.project_path.is_dir()
+        # looks for files in generated project
+        found_toplevel_files = [f.name for f in result.project_path.glob('*')]
+        assert '.github' in found_toplevel_files
+        assert  'dbt' in found_toplevel_files
+        assert 'setup.py' in found_toplevel_files
+        assert 'tox.ini' in found_toplevel_files
+        assert 'test' in found_toplevel_files
     
-
-def test_bake_project_fail(cookies):
+def test_bake_direcotry_name(cookies):
     """bake and test against new words, if passes means its changing values in template"""
-    result = cookies.bake()
-    assert result.exit_code == 0
-    assert result.exception is None
-    assert result.project_path.name != "hello_world"
-    assert result.project_path.is_dir()
-    # looks for files in generated project
-    found_toplevel_files = [f.name for f in result.project_path.glob('*')]
-    assert  'dbt' in found_toplevel_files
-    assert 'setup.py' in found_toplevel_files
-    assert 'tox.ini' in found_toplevel_files
-    assert 'test' in found_toplevel_files
+    with bake_in_temp_dir(cookies) as result:
+        assert result.exit_code == 0
+        assert result.exception is None
+        assert result.context['directory_name'] == 'myadapter'
+        assert result.context['directory_name'] != "hello_world"
+       
+def test_bake_deafult_adapter_src(cookies):
+    '''bake and test default version of adapter_src is sql'''
+    with bake_in_temp_dir(cookies) as result:
+        assert result.exit_code == 0
+        assert result.exception is None
+        assert result.context['adapter_src'] =='sql'
+        assert result.context['adapter_src'] != 'base'
+        assert result.context['adapter_src'] != '1'
+        assert result.context['adapter_src'] != '2'
 
-def test_without_internationalization(cookies):
-    """test that a directory d oes not exist deeply in generated project."""
-    with bake_in_temp_dir(
-        cookies,
-    ) as result:
-        assert result.project_path.joinpath('dbt/adapters/hello').is_dir() is False
+def test_bake_adapter_src_base(cookies):
+    '''bake and test adapter_src can register change from sql to base'''
+    with bake_in_temp_dir(cookies,extra_context={'adapter_src': 'base'}) as result:
+        assert result.exit_code == 0
+        assert result.exception is None
+        assert result.context['adapter_src'] == 'base'
+        assert result.context['adapter_src'] != 'sql'
 
 
+def test_bake_author_default(cookies):
+    with bake_in_temp_dir(cookies) as result:
+        assert result.context['author'] == "<INSERT AUTHOR HERE>"
 
+def test_bake_author_change(cookies):
+    with bake_in_temp_dir(cookies,extra_context={'author':'John Doe'}) as result:
+        assert result.context['author'] == 'John Doe'
+        assert result.context['author'] != 'JOhn Dooe'
+
+def test_bake_email_default(cookies):
+    with bake_in_temp_dir(cookies) as result:
+        assert result.context['author_email'] == "<INSERT EMAIL HERE>"
+
+def test_bake_email_change(cookies):
+    with bake_in_temp_dir(cookies,extra_context={'author_email':'John.D@test.com'}) as result:
+        assert result.context['author_email'] == 'John.D@test.com'
+        assert result.context['author_email'] != 'J@hn.d@test.com'
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        assert re.fullmatch(regex,result.context['author_email'])

d1eef2eee570333ed6eeec95c746f85b2ce8c022
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-02-04 14:56:34 -0600

    adding .gitkeep to inner directory

--- tests/template_test.py
+++ tests/template_test.py
@@ -20,6 +20,7 @@ def test_bake_project_default_adapter_name(cookies):
         assert result.project_path.is_dir()
         # looks for files in generated project
         found_toplevel_files = [f.name for f in result.project_path.glob('*')]
+        assert '.github' in found_toplevel_files
         assert  'dbt' in found_toplevel_files
         assert 'setup.py' in found_toplevel_files
         assert 'tox.ini' in found_toplevel_files

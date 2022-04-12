64a684d00feca2e3ed3810206cd49b98c6b5f918
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-04-12 10:58:59 -0500

    try just passing class

--- {{cookiecutter.project_name}}/tests/conftest.py
+++ {{cookiecutter.project_name}}/tests/conftest.py
@@ -1,26 +1,13 @@
-# Un comment and create your fixture for python test
+import pytest
+import os
+import json
 
-# import pytest
-# import os
-# import json
+# Import the fuctional fixtures as a plugin
+# Note: fixtures with session scope need to be local
 
-# # Import the fuctional fixtures as a plugin
-# # Note: fixtures with session scope need to be local
+pytest_plugins = ["dbt.tests.fixtures.project"]
 
-# pytest_plugins = ["dbt.tests.fixtures.project"]
-
-# # The profile dictionary, used to write out profiles.yml
-# @pytest.fixture(scope="class")
-# def dbt_profile_target():
-#     pass
-
-#     '''
-#     example of what return object might look like
-
-#     {
-#       type: {{cookiecutter.adapter_name}}
-#       user:
-#       database:
-#       etc..
-#     }
-#     '''
+# The profile dictionary, used to write out profiles.yml
+@pytest.fixture(scope="class")
+def dbt_profile_target():
+    pass

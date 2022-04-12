b27568dc94cada11dcae384fbc6532629d08088f
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-04-12 14:47:58 -0500

    trying different format for plugin

--- {{cookiecutter.project_name}}/tests/conftest.py
+++ {{cookiecutter.project_name}}/tests/conftest.py
@@ -5,7 +5,7 @@ import json
 # Import the fuctional fixtures as a plugin
 # Note: fixtures with session scope need to be local
 
-pytest_plugins = ["dbt.tests.fixtures.project"]
+pytest_plugins = ("dbt.tests.fixtures.project",)
 
 # The profile dictionary, used to write out profiles.yml
 @pytest.fixture(scope="class")

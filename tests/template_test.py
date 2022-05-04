a2329668262d12654cc14c20825a493353253b0c
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-05-04 16:27:43 -0500

    trying to run tests

--- tests/template_test.py
+++ tests/template_test.py
@@ -12,7 +12,7 @@ def bake_in_temp_dir(cookies, *args, **kwargs):
 
 def test_bake_project_adapter_name(cookies):
     """bake and test against default values of cookiecutter.json, adapter_name, and top level files created"""
-    with bake_in_temp_dir(cookies,extra_context={ "adapter_name": "MyAdapter" }) as result:
+    with bake_in_temp_dir(cookies, extra_context={ "adapter_name": "MyAdapter" }) as result:
         assert result.exit_code == 0
         assert result.exception is None
         assert result.context["adapter_name"] == "MyAdapter"
@@ -27,7 +27,7 @@ def test_bake_project_adapter_name(cookies):
 
 def test_bake_project_name(cookies):
     """bake and test against new words, if passes means its changing values in template"""
-    with bake_in_temp_dir(cookies,extra_context={ "adapter_name": "MyAdapter" }) as result:
+    with bake_in_temp_dir(cookies, extra_context={ "adapter_name":"MyAdapter" }) as result:
         assert result.exit_code == 0
         assert result.exception is None
         assert result.context["project_name"] == "dbt-myadapter"
@@ -41,7 +41,7 @@ def test_bake_deafult_is_sql_adapter(cookies):
 
 def test_bake_is_sql_adapter_base(cookies):
     """bake and test is_sql_adapter can register change from true to false"""
-    with bake_in_temp_dir(cookies,extra_context={ "is_sql_adapter": "false" }) as result:
+    with bake_in_temp_dir(cookies, extra_context={ "is_sql_adapter":"false" }) as result:
         assert result.exit_code == 0
         assert result.exception is None
         assert result.context["is_sql_adapter"] == "false"
@@ -51,7 +51,7 @@ def test_bake_author_default(cookies):
         assert result.context["author"] == "<INSERT AUTHOR HERE>"
 
 def test_bake_author_change(cookies):
-    with bake_in_temp_dir(cookies,extra_context={ "author":"John Doe" }) as result:
+    with bake_in_temp_dir(cookies, extra_context={ "author":"John Doe" }) as result:
         assert result.context["author"] == "John Doe"
 
 
@@ -60,7 +60,7 @@ def test_bake_email_default(cookies):
         assert result.context["author_email"] == "<INSERT EMAIL HERE>"
 
 def test_bake_email_change(cookies):
-    with bake_in_temp_dir(cookies,extra_context={ "author_email":"John.D@test.com" }) as result:
+    with bake_in_temp_dir(cookies, extra_context={ "author_email":"John.D@test.com" }) as result:
         assert result.context["author_email"] == "John.D@test.com"
         assert result.context["author_email"] != "J@hn.d@test.com"
         regex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
